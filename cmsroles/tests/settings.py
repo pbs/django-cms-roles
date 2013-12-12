22af62516a019e60ed3900962c9653b49bce44b4
Andrei Horak <linkyndy@gmail.com>
2013-12-12 16:54:15 +0200

    (tox) Base tox.ini and related changes
    Added base tox.ini file;
    Changed .gitignore and other old test-related files.

--- cmsroles/tests/settings.py
+++ cmsroles/tests/settings.py
@@ -14,16 +14,14 @@ INSTALLED_APPS = [
     'south',
     'sekizai',
     'cmsroles',
-    'django_nose',
 ]
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 CMS_TEMPLATES = [('cms_mock_template.html', 'cms_mock_template.html')]
 CMS_MODERATOR = True
 CMS_PERMISSION = True
 STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
-ROOT_URLCONF = 'urls_test'
+ROOT_URLCONF = 'cmsroles.tests.urls'
 TEMPLATE_CONTEXT_PROCESSORS = [
     "django.contrib.auth.context_processors.auth",
     'django.contrib.messages.context_processors.messages',
