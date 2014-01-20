307ffb4a4769f540f618fb4cd76d3b1e64054789
Laura Feier <laura.feier@3pillarglobal.com>
2014-01-20 14:03:51 +0200

    Bumped version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependency_links = [
 
 setup(
     name='django-cms-roles',
-    version='0.4.1',
+    version='0.4.2',
     description=('Wrapper over django-cms\' permissions that allows '
                  'for easy user management by defining roles that '
                  'span multiple sites.'),
