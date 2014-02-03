7f7f81e96d76c0598329e16823766ba24041a231
Bogdan Hodorog <bogdan.hodorog@3pillarglobal.com>
2014-02-03 16:58:41 +0200

    Bump version as instructed by bamboo

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ dependencies = [
 
 setup(
     name='django-cms-roles',
-    version='0.4.2',
+    version='0.4.3',
     description=('Wrapper over django-cms\' permissions that allows '
                  'for easy user management by defining roles that '
                  'span multiple sites.'),
