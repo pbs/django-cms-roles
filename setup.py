48e8854fb7213e9db9417116e4625396703cb543
bogdan.virtosu <virtosu.bogdan@gmail.com>
2016-10-18 10:22:54 +0300

    Bump version to 0.7.0.pbs.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setup(
     name='django-cms-roles',
-    version='0.7.0.pbs.6',
+    version='0.7.0.pbs.7',
     description=('Wrapper over django-cms\' permissions that allows '
                  'for easy user management by defining roles that '
                  'span multiple sites.'),
